d2336fc2ef34f9cce46d36c295ad5af66436b576
Dida En <den.elfsuju@gmail.com>
2022-01-16 12:37:20 +0700

    Mengganti id=password1, menambahkan attribute name=password1 pada field Password.

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -18,7 +18,7 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                    <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
                                 </div>
                                 <div class="col-sm-6">
                                     <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
