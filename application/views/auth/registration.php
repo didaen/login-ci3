d28a50b0b2c0bbf695a063b617b55432c046e95b
Dida En <den.elfsuju@gmail.com>
2022-01-16 13:02:29 +0700

    Mengubah <a> menjadi <button> pada tombol Register Account

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -24,9 +24,9 @@
                                     <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
                                 </div>
                             </div>
-                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                            <button href="login.html" class="btn btn-primary btn-user btn-block">
                                 Register Account
-                            </a>
+                            </button>
                             <hr>
                             <a href="index.html" class="btn btn-google btn-user btn-block">
                                 <i class="fab fa-google fa-fw"></i> Register with Google
