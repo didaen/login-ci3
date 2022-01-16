f83853f8ea6a9b71e04b7b8f37f889fc1598dbb1
Dida En <den.elfsuju@gmail.com>
2022-01-16 18:18:09 +0700

    Ketika data berhasil ditambahkan, cetak 'data berhasil ditambahkan'.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -25,11 +25,15 @@ class Auth extends CI_Controller
     public function registration()
     {
         // Kondisi form_validation
+        // Jika data gagal ditambahkan lakukan
         if ($this->form_validation->run() == false) {
             $data['title'] = 'Physics Yourself Registration';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
+        } else {
+            // Jika data berhasil ditambahkan
+            echo 'data berhasil ditambahakan!';
         }
     }
 }
