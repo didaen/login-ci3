22ff001707f10b7af9aced54641ec28406f3383e
Dida En <den.elfsuju@gmail.com>
2022-01-16 09:40:02 +0700

    Mengubah ukuran menjadi col-lg-7

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -27,13 +27,12 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-lg-7">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
