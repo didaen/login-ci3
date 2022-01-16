7247fac9585f18b0ddbae84a0ff96625d4a3d2d0
Dida En <den.elfsuju@gmail.com>
2022-01-16 17:51:41 +0700

    Menambahkan method constructor atau public function __contruct()yang akan selalu dipanggil saat memanggil controller Auth.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+    }
     public function index()
     {
         $this->load->view('templates/auth_header');
