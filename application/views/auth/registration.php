a3a6a2faef938022102f21472703f0b276fb52cf
Dida En <den.elfsuju@gmail.com>
2022-01-16 12:27:39 +0700

    Mencopy bagian <div> email yang secara tampilan sudah cocok seperti yang kita mau untuk dijadikan field name dan mempastekan di atasnya.

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -13,6 +13,9 @@
                             <div class="form-group">
                                 <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
                             </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                            </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
