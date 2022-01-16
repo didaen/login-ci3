57ec8ba13d9842a4b090847e03d101d04cabb0be
Dida En <den.elfsuju@gmail.com>
2022-01-16 18:15:10 +0700

    Apabila validasi gagal dilakukan, maka kembalikan ke form sebelumnya.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -26,11 +26,10 @@ class Auth extends CI_Controller
     {
         // Kondisi form_validation
         if ($this->form_validation->run() == false) {
+            $data['title'] = 'Physics Yourself Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
         }
-
-        $data['title'] = 'Physics Yourself Registration';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
     }
 }
