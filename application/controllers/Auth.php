fb0c25f2a9053b5232f55979246bee0766bbf655
Dida En <den.elfsuju@gmail.com>
2022-01-16 22:32:56 +0700

    TERNYATA MASIH HARUS MENGATUR set_rules() untuk name=password2. Perbedaannya tentu ada di 'name' yang diisi dengan 'password2', TIDAK ADA min_length karena sudah mengikuti yang set_rules name=password1, bagian matches[password1].

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -31,6 +31,8 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
         // 3. Password, required (tidak boleh kosong)
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
+        // 3. Repeat Password Password, required (tidak boleh kosong)
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         // Kondisi form_validation
         // Jika data gagal ditambahkan lakukan
