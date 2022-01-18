d19b60deefa63ec917e396a1bd4483cd20911f3f
Dida En <den.elfsuju@gmail.com>
2022-01-18 15:11:58 +0700

    Menambahkan key 'title' pada array data pada controller Auth dan method index() atau pada halaman login.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,6 +17,7 @@ class Auth extends CI_Controller
     }
     public function index()
     {
+        $data['title'] = 'Login Page';
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
